1b187aadfc428b7243eeeefd66abc6e664a3b69a
Gabriel R T Araújo <grt.araujo@hotmail.com>
2022-05-24 12:46:41 -0300

    Small changes

--- exercise1.sh
+++ exercise1.sh
@@ -1,9 +1,8 @@
 (
-    time=0
+    #time=0
     for i in {0..9}; do
-        time=$(($time+5))
-        xterm -e "bash -c 'timeout $time bin/connectTo $1 <$2';bash" &
+        #time=$(($time+10))
+        xterm -e "bash -c 'echo \"Hi from client $(($i+1))!\" | bin/connectTo $1;bash'" &
         sleep 1
     done
-);
-
+)
